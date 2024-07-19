63b0e7e993add5868f6c84b37431fdbae242cec9
INIT-REF <rufmichan@freenet.de>
2024-07-19 06:15:55 +0200

    Update

---
+++ 020.cbl
@@ -0,0 +1,31 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Euler016.
+       
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 carry    PIC 9(3)    COMP    VALUE 0.
+       01 tmp      PIC 9(3)    COMP.
+       01 res      PIC 9(4)    COMP    VALUE 0.
+       01 n        PIC 9(3)    COMP.
+       01 fac100.
+           05 digs PIC 9       COMP    VALUE 0 OCCURS 200 TIMES 
+                                       INDEXED BY i.
+       01 out      PIC Z(10).
+
+       PROCEDURE DIVISION.
+           MOVE 1 to digs(1)
+
+           PERFORM VARYING n FROM 1 BY 1 UNTIL n > 100
+               PERFORM VARYING i FROM 1 BY 1 UNTIL i > 200
+                   COMPUTE tmp = n * digs(i) + carry
+                   DIVIDE tmp BY 10 GIVING carry REMAINDER digs(i)
+               END-PERFORM
+           END-PERFORM
+
+           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 200
+               ADD digs(i) TO res
+           END-PERFORM
+ 
+           MOVE res to out
+           DISPLAY FUNCTION TRIM (out)
+           STOP RUN.
