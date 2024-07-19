4be897566a3345c3f5ae60c94949f53574b0e381
INIT-REF <rufmichan@freenet.de>
2024-07-19 10:00:58 +0200

    Update

--- 025.cbl
+++ 025.cbl
@@ -12,8 +12,7 @@
        01 f2.
            05 fib2 PIC 9(10)   COMP   VALUE 0 OCCURS 100 TIMES 
                                        INDEXED BY j.
-       01 ftmp.
-           05 ft   PIC 9(10)   COMP   OCCURS 100 TIMES.
+       01 ftmp     PIC 9(10)   COMP.
        01 out      PIC Z(10).
 
        PROCEDURE DIVISION.
@@ -21,16 +20,15 @@
            MOVE 1 to fib2(1)
 
            PERFORM UNTIL fib2(100) > 1000000000
-               MOVE f2 to ftmp
-
                PERFORM VARYING i FROM 1 BY 1 UNTIL i > 100
                    SET j to i
+                   MOVE fib2(j) TO ftmp
                    COMPUTE tmp = fib1(i) + fib2(j) + carry
                    DIVIDE tmp BY 10000000000
                        GIVING carry REMAINDER fib2(j)
+                   MOVE ftmp to fib1(i)
                END-PERFORM
 
-               MOVE ftmp to f1
                ADD 1 to n
            END-PERFORM
  
