8ec4d33b178d843082b8ec08e1dd163d057e2c47
INIT-REF <rufmichan@freenet.de>
2024-07-19 06:18:55 +0200

    Update

--- 020.cbl
+++ 020.cbl
@@ -8,7 +8,7 @@
        01 res      PIC 9(4)    COMP    VALUE 0.
        01 n        PIC 9(3)    COMP.
        01 fac100.
-           05 digs PIC 9       COMP    VALUE 0 OCCURS 200 TIMES 
+           05 digs PIC 9       COMP    VALUE 0 OCCURS 158 TIMES 
                                        INDEXED BY i.
        01 out      PIC Z(10).
 
@@ -16,13 +16,13 @@
            MOVE 1 to digs(1)
 
            PERFORM VARYING n FROM 1 BY 1 UNTIL n > 100
-               PERFORM VARYING i FROM 1 BY 1 UNTIL i > 200
+               PERFORM VARYING i FROM 1 BY 1 UNTIL i > 158
                    COMPUTE tmp = n * digs(i) + carry
                    DIVIDE tmp BY 10 GIVING carry REMAINDER digs(i)
                END-PERFORM
            END-PERFORM
 
-           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 200
+           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 158
                ADD digs(i) TO res
            END-PERFORM
  
